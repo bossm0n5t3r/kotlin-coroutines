eae05296443d7964a63ca95d847a51718573ff0d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-02-06 03:20:10 +0900

    chapter15

--- src/test/kotlin/me/bossm0n5t3r/coroutines/chapter15/FetchUserDataTest.kt
+++ src/test/kotlin/me/bossm0n5t3r/coroutines/chapter15/FetchUserDataTest.kt
@@ -1,10 +1,29 @@
 package me.bossm0n5t3r.coroutines.chapter15
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.currentTime
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class FetchUserDataTest {
+    @Test
+    fun `should load data concurrently`() =
+        runTest {
+            // given
+            val userRepo = FakeUserDataRepository()
+            val useCase = FetchUserUseCase(userRepo)
+
+            // when
+            useCase.fetchUserData()
+
+            // then
+            assertEquals(1000, currentTime)
+        }
+
     @Test
     fun `should construct user`() =
         runBlocking {
@@ -26,10 +45,19 @@ class FetchUserDataTest {
         }
 
     class FakeUserDataRepository : UserDataRepository {
-        override suspend fun getName(): String = "Ben"
+        override suspend fun getName(): String {
+            delay(1000)
+            return "Ben"
+        }
 
-        override suspend fun getFriends(): List<Friend> = listOf(Friend("some-friend-id-1"))
+        override suspend fun getFriends(): List<Friend> {
+            delay(1000)
+            return listOf(Friend("some-friend-id-1"))
+        }
 
-        override suspend fun getProfile(): Profile = Profile("Example description")
+        override suspend fun getProfile(): Profile {
+            delay(1000)
+            return Profile("Example description")
+        }
     }
 }
