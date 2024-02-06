b52e9cd84e61f75a35ebad19f592d498827eda16
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-02-07 03:03:00 +0900

    chapter15

---
+++ src/test/kotlin/me/bossm0n5t3r/coroutines/chapter15/MockTest.kt
@@ -0,0 +1,50 @@
+package me.bossm0n5t3r.coroutines.chapter15
+
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.currentTime
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class MockTest {
+    private fun generateRandomString() = UUID.randomUUID().toString()
+
+    @Test
+    fun `should load data concurrently`() =
+        runTest {
+            // given
+            val userRepo = mockk<UserDataRepository>()
+            val aName = generateRandomString()
+            val someFriends =
+                listOf(
+                    Friend(generateRandomString()),
+                    Friend(generateRandomString()),
+                    Friend(generateRandomString()),
+                )
+            val aProfile = Profile(generateRandomString())
+            coEvery { userRepo.getName() } coAnswers {
+                delay(600)
+                aName
+            }
+            coEvery { userRepo.getFriends() } coAnswers {
+                delay(700)
+                someFriends
+            }
+            coEvery { userRepo.getProfile() } coAnswers {
+                delay(800)
+                aProfile
+            }
+            val useCase = FetchUserUseCase(userRepo)
+
+            // when
+            useCase.fetchUserData()
+
+            // then
+            assertEquals(800, currentTime)
+        }
+}
