9d246bb2ab494cdf1976e8f2795e9440e52a5d83
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-02-07 02:33:00 +0900

    chapter15

--- src/test/kotlin/me/bossm0n5t3r/coroutines/chapter15/MapAsyncTest.kt
+++ src/test/kotlin/me/bossm0n5t3r/coroutines/chapter15/MapAsyncTest.kt
@@ -1,14 +1,21 @@
 package me.bossm0n5t3r.coroutines.chapter15
 
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.job
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.currentTime
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.withContext
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.coroutines.CoroutineContext
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class MapAsyncTest {
@@ -44,5 +51,49 @@ class MapAsyncTest {
             val res = transforms.mapAsync { it() }
             assertEquals(listOf("A", "B", "C", "D"), res)
             assertEquals(4000, currentTime)
+        }
+
+    @Test
+    fun `should support context propagation`() =
+        runTest {
+            var ctx: CoroutineContext? = null
+
+            val name1 = CoroutineName("Name 1")
+            withContext(name1) {
+                listOf("A").mapAsync {
+                    // in transformation
+                    ctx = currentCoroutineContext() // should be name1
+                    it
+                }
+                assertEquals(name1, ctx?.get(CoroutineName))
+            }
+
+            val name2 = CoroutineName("Some name 2")
+            withContext(name2) {
+                listOf(1, 2, 3).mapAsync {
+                    // in transformation
+                    ctx = currentCoroutineContext() // should be name2
+                    it
+                }
+                assertEquals(name2, ctx?.get(CoroutineName))
+            }
+        }
+
+    @Test
+    fun `should support cancellation`() =
+        runTest {
+            var job: Job? = null
+
+            val parentJob =
+                launch {
+                    listOf("A").mapAsync {
+                        job = currentCoroutineContext().job // refer to a job
+                        delay(Long.MAX_VALUE)
+                    }
+                }
+
+            delay(1000)
+            parentJob.cancel()
+            assertEquals(true, job?.isCancelled) // referred job should be cancelled
         }
 }
