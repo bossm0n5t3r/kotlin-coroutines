9fe12d7b75262fbed25939cf0b36b5550bfe6b3a
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-02-08 12:05:00 +0900

    chapter15

---
+++ src/test/kotlin/me/bossm0n5t3r/coroutines/chapter15/SendNotificationsTest.kt
@@ -0,0 +1,117 @@
+package me.bossm0n5t3r.coroutines.chapter15
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.currentTime
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class SendNotificationsTest {
+    data class Notification(val id: Int)
+
+    interface NotificationsService {
+        val notificationsSent: MutableSet<Notification>
+
+        suspend fun send(notification: Notification)
+    }
+
+    class FakeNotificationsService(
+        private val delayMillis: Long,
+    ) : NotificationsService {
+        override val notificationsSent: MutableSet<Notification> = mutableSetOf()
+
+        override suspend fun send(notification: Notification) {
+            notificationsSent.add(notification)
+            delay(delayMillis)
+        }
+    }
+
+    interface NotificationsRepository {
+        val notificationsMarkedAsSent: MutableSet<Int>
+
+        fun notificationsToSend(): List<Notification>
+
+        suspend fun markAsSent(id: Int)
+    }
+
+    class FakeNotificationsRepository(
+        private val delayMillis: Long,
+        private val notifications: List<Notification>,
+    ) : NotificationsRepository {
+        override val notificationsMarkedAsSent: MutableSet<Int> = mutableSetOf()
+
+        override fun notificationsToSend(): List<Notification> {
+            return notifications
+        }
+
+        override suspend fun markAsSent(id: Int) {
+            notificationsMarkedAsSent.add(id)
+            delay(delayMillis)
+        }
+    }
+
+    class NotificationsSender(
+        private val notificationsRepository: NotificationsRepository,
+        private val notificationsService: NotificationsService,
+        private val notificationsScope: CoroutineScope,
+    ) {
+        fun sendNotifications() {
+            notificationsScope.launch {
+                val notifications =
+                    notificationsRepository
+                        .notificationsToSend()
+                for (notification in notifications) {
+                    launch {
+                        notificationsService.send(notification)
+                        notificationsRepository
+                            .markAsSent(notification.id)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun testSendNotifications() {
+        // given
+        val notifications = List(100) { Notification(it) }
+        val repo =
+            FakeNotificationsRepository(
+                delayMillis = 200,
+                notifications = notifications,
+            )
+        val service =
+            FakeNotificationsService(
+                delayMillis = 300,
+            )
+        val testScope = TestScope()
+        val sender =
+            NotificationsSender(
+                notificationsRepository = repo,
+                notificationsService = service,
+                notificationsScope = testScope,
+            )
+
+        // when
+        sender.sendNotifications()
+        testScope.advanceUntilIdle()
+
+        // then all notifications are sent and marked
+        assertEquals(
+            notifications.toSet(),
+            service.notificationsSent.toSet(),
+        )
+        assertEquals(
+            notifications.map { it.id }.toSet(),
+            repo.notificationsMarkedAsSent.toSet(),
+        )
+
+        // and notifications are sent concurrently
+        assertEquals(500, testScope.currentTime) // FIXME expected should be 700, why?
+    }
+}
